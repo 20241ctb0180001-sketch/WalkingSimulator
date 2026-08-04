4532bac7c3c5e9445ca66e81253dcd203828ef44
0180001-sketch-ViinPerez <20233024265@estudantes.ifpr.edu.br>
2026-08-04 15:31:28 -0300

    bilu
    deixando anotado, vc pode tirar tudo q eu adicionar, nn mexi em nada do projeto original, so adicionei

--- MyWalkingSim/Assets/Scripts/AbrirTampinha.cs
+++ MyWalkingSim/Assets/Scripts/AbrirTampinha.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using TMPro;
 
 public class AbrirTampinha : MonoBehaviour
 {
     
     private Animator anim;
+
+    private bool taEmAlcance = false;
+
+    [SerializeField] private TextMeshProUGUI codetxt;
+    string codeTxtvalue = "";
+    public string senha;
+    public GameObject painelcdg;
 
     void Start()
     {
@@ -12,6 +20,41 @@ public class AbrirTampinha : MonoBehaviour
 
     void Update()
     {
-        
+        codetxt.text = codeTxtvalue;
+
+        if(codeTxtvalue == senha)
+        {
+            anim.SetTrigger("abrirtampa");
+            painelcdg.SetActive(false);
+        }
+
+        if(codeTxtvalue.Length >= 6)
+        {
+            codeTxtvalue = "";
+        }
+
+        if(Input.GetKey(KeyCode.E) && taEmAlcance == true)
+        {
+            painelcdg.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            taEmAlcance = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        taEmAlcance = false;
+        painelcdg.SetActive(false);
+    }
+
+    public void AddDigito(string digito)
+    {
+        codeTxtvalue += digito;
     }
 }
